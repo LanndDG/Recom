e0e32615ba5b7e26c74189feaa20a98b45c85a89
bui duc hoang <hoang@buis-MacBook-Air.local>
2020-10-26 17:59:02 +0700

    hoang/fish-company2

--- js/script.js
+++ js/script.js
@@ -168,12 +168,11 @@ form.addEventListener('input', () => {
     メールアドレス.value.length > 0 && 
     電話番号.value.length > 0 && 
     お問い合わせ内容.value.length > 0) {
-    console.log('aaaa');
     submit.removeAttribute('disabled');
-    submit.setAttribute('style', 'background-color: rgb(245,136,93)')
+    submit.setAttribute('style', 'background-color: rgb(245,136,93);cursor: pointer;color: #ffffff');
   }else {
     submit.setAttribute('disabled', 'disabled');
-    submit.setAttribute('style', 'background-color: rgb(175,175,175)')
+    submit.setAttribute('style', 'background-color: rgb(175,175,175);cursor: default');
   }
 })
 
