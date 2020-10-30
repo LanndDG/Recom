4fb9a1cf4a496aef10d99f0b9202da65a11865bc
LanndDG <lan.nguyen@digitalgate.world>
2020-10-30 16:23:34 +0700

    change mail

--- lib/mail/mail.php
+++ lib/mail/mail.php
@@ -39,11 +39,11 @@ if (version_compare(PHP_VERSION, '5.1.0', '>=')) {//PHP5.1.0以上の場合の�
 $site_top = "http://webcircle09.heteml.net/demo/dg/Recom/";
 
 //管理者のメールアドレス ※メールを受け取るメールアドレス(複数指定する場合は「,」で区切ってください 例 $to = "aa@aa.aa,bb@bb.bb";)
-$to = "lan.nguyen@digitalgate.world";
+$to = "direction@webcircle.co.jp";
 
 //自動返信メールの送信元メールアドレス
 //必ず実在するメールアドレスでかつ出来る限り設置先サイトのドメインと同じドメインのメールアドレスとすることを強く推奨します
-$from = "lan.nguyen@digitalgate.world";
+$from = "direction@webcircle.co.jp";
 
 //フォームのメールアドレス入力箇所のname属性の値（name="○○"　の○○部分）
 $Email = "メールアドレス";
